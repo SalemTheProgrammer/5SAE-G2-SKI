1a694948f689be6127fa947f42716f17723d1ada
rahmouni29301 <rahmouni.hamza@esprit.tn>
2024-11-02 03:12:29 +0100

    docker-compose

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -1,15 +1,9 @@
 package tn.esprit.spring.repositories;
 
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.data.repository.query.Param;
 import tn.esprit.spring.entities.Subscription;
-import tn.esprit.spring.entities.TypeSubscription;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
 
 public interface ISubscriptionRepository extends CrudRepository<Subscription, Long> {
 
