dc6b6e9867491df5dbe1ebf327b4865d336a3fac
Salem Dahmani <salem.dahmani345@gmail.com>
2024-11-04 13:47:58 +0100

    added docker compose file

---
+++ src/main/java/tn/esprit/spring/configs/LoggingAspect.java
@@ -0,0 +1,26 @@
+package tn.esprit.spring.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before(" execution(* tn.esprit.spring.services.*.*(..)) ")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        System.out.println("In method " + name + " : ");
+    }
+
+    @After(" execution(* tn.esprit.spring.services.*.*(..)) ")
+    public void logMethodExit(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        System.out.println("Out of method " + name + " : ");
+    }
+}
