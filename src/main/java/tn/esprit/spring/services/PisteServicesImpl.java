41ef517721408533b60acbb22a8784eb22ec066c
Dhiaeddine Uwobikundiye <dhiaeddine.uwobikundiye@esprit.tn>
2024-11-07 22:26:48 +0100

    tests update

--- src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
@@ -15,23 +15,26 @@ public class PisteServicesImpl implements  IPisteServices{
 
     @Override
     public List<Piste> retrieveAllPistes() {
+        /*
         logger.info("Starting retrieval of all pistes");
         List<Piste> pistes = pisteRepository.findAll();
         logger.debug("Number of pistes retrieved: {}", pistes.size());
+        */
+
         return pisteRepository.findAll();
     }
 
     @Override
     public Piste addPiste(Piste piste) {
-        logger.info("Attempting to add new piste with name: {}", piste.getNamePiste());
-        try {
+       // logger.info("Attempting to add new piste with name: {}", piste.getNamePiste());
+        //try {
            // Piste savedPiste = pisteRepository.save(piste);
           //  logger.info("Piste added successfully with ID: {}", savedPiste.getNumPiste());
         return pisteRepository.save(piste);
-        } catch (Exception e) {
-            logger.error("Error adding piste: {}", e.getMessage(), e);
-            throw e;
-        }
+       // } catch (Exception e) {
+         //   logger.error("Error adding piste: {}", e.getMessage(), e);
+          //  throw e;
+       // }
     }
 
     @Override
@@ -41,27 +44,27 @@ public class PisteServicesImpl implements  IPisteServices{
 
     @Override
     public void removePiste(Long numPiste) {
-        logger.info("Attempting to remove piste with ID: {}", numPiste);
-        if (pisteRepository.existsById(numPiste)) {
-            pisteRepository.deleteById(numPiste);
-            logger.info("Piste with ID: {} removed successfully", numPiste);
-        } else {
-            logger.warn("Piste with ID: {} does not exist", numPiste);
-        }
+      //  logger.info("Attempting to remove piste with ID: {}", numPiste);
+        //if (pisteRepository.existsById(numPiste)) {
+           // pisteRepository.deleteById(numPiste);
+           // logger.info("Piste with ID: {} removed successfully", numPiste);
+      //  } else {
+       //     logger.warn("Piste with ID: {} does not exist", numPiste);
+       // }
         pisteRepository.deleteById(numPiste);
     }
 
 
     @Override
     public Piste retrievePiste(Long numPiste) {
-        logger.info("Retrieving piste with ID: {}", numPiste);
-        //return pisteRepository.findById(numPiste).orElse(null);
+       // logger.info("Retrieving piste with ID: {}", numPiste);
+        return pisteRepository.findById(numPiste).orElse(null);
 
-        return pisteRepository.findById(numPiste).orElseGet(() -> {
-            logger.warn("Piste with ID: {} not found", numPiste);
-            return null;
-        });
-    }
+        //return pisteRepository.findById(numPiste).orElseGet(() -> {
+          //  logger.warn("Piste with ID: {} not found", numPiste);
+         //   return null;
+     //   });
+   }
 }
 
 
